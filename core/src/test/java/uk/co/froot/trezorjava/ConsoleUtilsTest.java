abe7b28969d94410880a34af86df80dd46d81afe
Gary Rowe <g.rowe@froot.co.uk>
2018-04-30 16:45:15 +0100

    Use assertThat pattern for testing

--- core/src/test/java/uk/co/froot/trezorjava/ConsoleUtilsTest.java
+++ core/src/test/java/uk/co/froot/trezorjava/ConsoleUtilsTest.java
@@ -3,7 +3,8 @@ package uk.co.froot.trezorjava;
 import org.junit.Test;
 import uk.co.froot.trezorjava.core.utils.ConsoleUtils;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ConsoleUtilsTest {
 
@@ -20,7 +21,7 @@ public class ConsoleUtilsTest {
 
     String result = ConsoleUtils.formatBytesAsHex(bytes);
 
-    assertEquals("\n\u001B[32m" +
+    assertThat(result, is("\n\u001B[32m" +
       " 0  1  2  3  4  5  6  7  8  9 " +
       "10 11 12 13 14 15 16 17 18 19 " +
       "20 21 22 23 24 25 26 27 28 29 " +
@@ -28,8 +29,7 @@ public class ConsoleUtilsTest {
       "40 41 42 43 44 45 46 47 48 49 " +
       "50 51 52 53 54 55 56 57 58 59 " +
       "60 61 62 63\n\u001B[0m" +
-      "00 01 02 03 04 05 06 07 08 09 ",
-      result
+      "00 01 02 03 04 05 06 07 08 09 ")
     );
 
   }
@@ -47,7 +47,7 @@ public class ConsoleUtilsTest {
 
     String result = ConsoleUtils.formatBytesAsHex(bytes);
 
-    assertEquals("\n\u001B[32m" +
+    assertThat(result, is("\n\u001B[32m" +
         " 0  1  2  3  4  5  6  7  8  9 " +
         "10 11 12 13 14 15 16 17 18 19 " +
         "20 21 22 23 24 25 26 27 28 29 " +
@@ -62,8 +62,7 @@ public class ConsoleUtilsTest {
         "28 29 2A 2B 2C 2D 2E 2F 30 31 " +
         "32 33 34 35 36 37 38 39 3A 3B " +
         "3C 3D 3E 3F\n" +
-        "40 41 42 43 44 45 46 47 48 49 ",
-      result
+        "40 41 42 43 44 45 46 47 48 49 ")
     );
 
   }

ed6d234da97b4cfed9acb0c6b13529dec1f8e183
Gary Rowe <g.rowe@froot.co.uk>
2018-05-01 12:18:12 +0100

    Remove service module Main contents for now

--- service/src/main/java/uk/co/froot/trezorjava/service/Main.java
+++ service/src/main/java/uk/co/froot/trezorjava/service/Main.java
@@ -1,12 +1,7 @@
 package uk.co.froot.trezorjava.service;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Message;
-import com.satoshilabs.trezor.lib.protobuf.TrezorMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.co.froot.trezorjava.core.TrezorManager;
-import uk.co.froot.trezorjava.core.utils.ConsoleUtils;
 
 public class Main {
 
@@ -14,17 +9,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-    TrezorManager trezorManager = new TrezorManager();
-    trezorManager.initialise();
-
-    try {
-      Message response = trezorManager.sendPing();
-      if (response instanceof TrezorMessage.Success) {
-        System.out.println(ConsoleUtils.ANSI_GREEN + "SUCCESS" + ConsoleUtils.ANSI_RESET + " Message:" + ((TrezorMessage.Success) response).getMessage());
-      }
-    } catch (InvalidProtocolBufferException e) {
-      e.printStackTrace();
-    }
+    // TODO Add high level API
 
   }
 
